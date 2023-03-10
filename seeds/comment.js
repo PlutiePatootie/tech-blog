3979988b5a31615154ad7b39c057479a94b4b36a
PlutiePatootie <119901591+PlutiePatootie@users.noreply.github.com>
2023-03-10 08:38:35 -0500

    Update comment.js

--- seeds/comment.js
+++ seeds/comment.js
@@ -2,49 +2,49 @@ const { Comment } = require('../models');
 
 const commentData = [
     {
-        comment_text: "No way!",
+        comment_text: "12313",
         user_id: 2,
         post_id: 3,
         
     },
     {
-        comment_text: "First Comment :)",
+        comment_text: " 124141",
         user_id: 2,
         post_id: 5,
         
     },
     {
-        comment_text: "Is this believable?",
+        comment_text: "123",
         user_id: 4,
         post_id: 1,
         
     },
     {
-        comment_text: "Nice.",
+        comment_text: "1241.",
         user_id: 3,
         post_id: 5,
         
     },
     {
-        comment_text: "Groundbreaking stuff, going to follow this post closely.",
+        comment_text: "412412",
         user_id: 3,
         post_id: 4,
         
     },
     {
-        comment_text: "As if!",
+        comment_text: "14412",
         user_id: 2,
         post_id: 1,
         
     },
     {
-        comment_text: "Right, you're entitled to your own opinion.",
+        comment_text: "14421214",
         user_id: 5,
         post_id: 3,
         
     },
     {
-        comment_text: "Awesome, I'd like to know more.",
+        comment_text: "144122414",
         user_id: 2,
         post_id: 1,
         
